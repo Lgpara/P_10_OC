7197c467ad87352a8c1159ca1c3c34330e327a00
Louis Gerber <louisgerber004@gmail.com>
2023-10-02 16:56:49 +0200

    cloned corrected version with forked unbugged version

--- src/containers/Events/index.js
+++ src/containers/Events/index.js
@@ -10,27 +10,35 @@ import "./style.css";
 const PER_PAGE = 9;
 
 const EventList = () => {
+
+
+  
+
   const { data, error } = useData();
-  const [type, setType] = useState();
+  const [type, setType] = useState("all");
+
+
   const [currentPage, setCurrentPage] = useState(1);
   const filteredEvents = (
     (!type
       ? data?.events
       : data?.events) || []
-  ).filter((event, index) => {
+  ).filter((event) => {
     if (
-      (currentPage - 1) * PER_PAGE <= index &&
-      PER_PAGE * currentPage > index
+      event.type === type
     ) {
       return true;
     }
     return false;
   });
+
+  const activeEvents = (type === "all" ? data?.events : filteredEvents)
+  const displayedEvents = activeEvents?.filter((_, index) => index >= (currentPage*PER_PAGE) - 9 && index < (currentPage*PER_PAGE));
   const changeType = (evtType) => {
     setCurrentPage(1);
     setType(evtType);
   };
-  const pageNumber = Math.floor((filteredEvents?.length || 0) / PER_PAGE) + 1;
+  const pageNumber = Math.ceil((activeEvents?.length || 0) / PER_PAGE);
   const typeList = new Set(data?.events.map((event) => event.type));
   return (
     <>
@@ -42,10 +50,10 @@ const EventList = () => {
           <h3 className="SelectTitle">Catégories</h3>
           <Select
             selection={Array.from(typeList)}
-            onChange={(value) => (value ? changeType(value) : changeType(null))}
+            onChange={(value) => (value ? changeType(value) : changeType("all"))}
           />
           <div id="events" className="ListContainer">
-            {filteredEvents.map((event) => (
+            {displayedEvents?.map((event) => (
               <Modal key={event.id} Content={<ModalEvent event={event} />}>
                 {({ setIsOpened }) => (
                   <EventCard
--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -12,8 +12,10 @@ const Slider = () => {
   );
   const nextCard = () => {
     setTimeout(
-      () => setIndex(index < byDateDesc.length ? index + 1 : 0),
-      5000
+      () => {
+        if(byDateDesc != null)
+        setIndex(index < byDateDesc.length - 1 ? index + 1 : 0)
+      },5000
     );
   };
   useEffect(() => {
@@ -42,10 +44,11 @@ const Slider = () => {
             <div className="SlideCard__pagination">
               {byDateDesc.map((_, radioIdx) => (
                 <input
-                  key={`${event.id}`}
+                  key={_.date}
                   type="radio"
                   name="radio-button"
-                  checked={idx === radioIdx}
+                  checked={index === radioIdx}
+                  onChange={()=>setIndex(radioIdx)}
                 />
               ))}
             </div>
